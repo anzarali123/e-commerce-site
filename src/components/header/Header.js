6b163af49b1a16ec439957cb0f6f06f84c029a32
Anzar Ali <anzarali150799@gmail.com>
2022-12-21 15:48:43 +0530

    built the header component and made it responsive as well

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,7 +1,82 @@
-import React from "react";
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./Header.module.scss";
+import { FaShoppingCart, FaTimes } from "react-icons/fa";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+
+const logo = (
+  <Link to="/">
+    <h2>
+      shop<span>Easy</span>
+    </h2>
+  </Link>
+);
+
+const cart = (
+  <span className={styles.cart}>
+    <Link to="/cart">
+      Cart <FaShoppingCart size={20} />
+      <p>0</p>
+    </Link>
+  </span>
+);
 
 const Header = () => {
-  return <h1 className="header">Header</h1>;
+  const [showMenu, setShowMenu] = useState(false);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
+  const hideMenu = () => {
+    setShowMenu(false);
+  };
+
+  return (
+    <header>
+      <div className={styles.header}>
+        <div className={styles.logo}>{logo}</div>
+        <nav
+          className={
+            showMenu ? `${styles["show-nav"]}` : `${styles["hide-nav"]}`
+          }
+        >
+          <div
+            className={
+              showMenu
+                ? `${styles["nav-wrapper"]} ${styles["show-nav-wrapper"]}`
+                : `${styles["nav-wrapper"]}`
+            }
+            onClick={hideMenu}
+          ></div>
+          <ul onClick={hideMenu}>
+            <li className={styles["logo-mobile"]}>
+              {logo} <FaTimes size={22} color="#fff" onClick={hideMenu} />
+            </li>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact Us</Link>
+            </li>
+          </ul>
+          <div className={styles["header-right"]} onClick={hideMenu}>
+            <span className={styles.links}>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+              <Link to="/order-history">My Orders</Link>
+            </span>
+            {cart}
+          </div>
+        </nav>
+
+        <div className={styles["menu-icon"]}>
+          {cart}
+          <HiOutlineMenuAlt3 size={28} onClick={toggleMenu} />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
