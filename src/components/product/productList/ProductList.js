2a5fecbd6377215e26cbc0b086849ccda5005124
Anzar Ali <anzarali150799@gmail.com>
2022-12-28 11:13:07 +0530

    built the search component

--- src/components/product/productList/ProductList.js
+++ src/components/product/productList/ProductList.js
@@ -1,7 +1,46 @@
 import styles from "./ProductList.module.scss";
+import { useState } from "react";
+import { BsFillGridFill } from "react-icons/bs";
+import { FaListAlt } from "react-icons/fa";
+import Search from "../../search/Search";
 
 const ProductList = () => {
-  return <div>productList</div>;
+  const [grid, setGrid] = useState(true);
+  const [search, setSearch] = useState("");
+
+  return (
+    <div className={styles["product-list"]} id="product">
+      <div className={styles.top}>
+        <div className={styles.icon}>
+          <BsFillGridFill
+            style={{ marginRight: "10px" }}
+            size={22}
+            color="orangered"
+            onClick={() => setGrid(true)}
+          />
+          <FaListAlt size={24} color="#0066d4" onClick={() => setGrid(false)} />
+          <p>
+            <b>10</b>Products found.
+          </p>
+        </div>
+        {/* Search Icon  */}
+        <div>
+          <Search value={search} onChange={(e) => setSearch(e.target.value)} />
+        </div>
+        {/* Sort List  */}
+        <div className={styles.sort}>
+          <label>Sort by:</label>
+          <select>
+            <option value="latest">Latest</option>
+            <option value="lowest-price">Lowest Price</option>
+            <option value="highest-price">Highest Price</option>
+            <option value="a-z">A-Z</option>
+            <option value="z-a">Z-A</option>
+          </select>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProductList;
