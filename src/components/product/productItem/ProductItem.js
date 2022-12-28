1e317f4cf35bfaba0f060751177ca7e9b8897117
Anzar Ali <anzarali150799@gmail.com>
2022-12-28 11:36:11 +0530

    built the product item component

--- src/components/product/productItem/ProductItem.js
+++ src/components/product/productItem/ProductItem.js
@@ -1,10 +1,30 @@
 import styles from "./ProductItem.module.scss";
-
-const ProductItem = () => {
+import { Card } from "../../index";
+import { Link } from "react-router-dom";
+const ProductItem = ({ product, grid, id, name, price, desc, imageURL }) => {
+  const shortenText = (text, n) => {
+    if (text.length > n) {
+      const shortenedText = text.substring(0, n).concat("...");
+      return shortenedText;
+    }
+    return text;
+  };
   return (
-    <div>
-      <h1>productItem</h1>
-    </div>
+    <Card cardClass={grid ? `${styles.grid}` : `${styles.list}`}>
+      <Link to={`/product-details`}>
+        <div className={styles.img}>
+          <img src={imageURL} alt={name} />
+        </div>
+      </Link>
+      <div className={styles.content}>
+        <div className={styles.details}>
+          <p>{`$${price}`}</p>
+          <h4>{shortenText(name, 18)}</h4>
+        </div>
+        {!grid && <p className={styles.desc}>{shortenText(desc, 200)}</p>}
+        <button className="--btn --btn-danger">Add To Cart</button>
+      </div>
+    </Card>
   );
 };
 
